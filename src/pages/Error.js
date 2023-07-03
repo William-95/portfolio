5b09441cac77bf02ccd379bb1b44dbefa143e973
William-95 <verga.william.95@gmail.com>
2023-07-03 16:10:31 +0200

    a

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,23 @@
 import React from "react";
+import headerBg from "../img/header3.jpg";
+import error from "../img/error.png";
+import "../css/error.css";
 
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <>
+      
+
+      <div className="error"  style={{
+          backgroundImage: `url(${headerBg})`,
+        }}>
+        <img src={error} className="errorImg" alt="" />
+        <h4>Abbiamo riscontrato un errore, prova:</h4>
+        <ol>
+          <li>Controlla la tua connessione</li>
+          <li>Ricarica la pagina</li>
+        </ol>
+      </div>
+    </>
+  );
 }
