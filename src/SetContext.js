22bef898051e8d82f45d60d31662ddc05f504871
William-95 <verga.william.95@gmail.com>
2023-11-28 21:05:32 +0100

    Update SetContext.js

--- src/SetContext.js
+++ src/SetContext.js
@@ -76,42 +76,15 @@ export default function SetContext({ children }) {
   const [project] = useState([
     {
       id: 0,
-      img: portfolio,
-      name: "FirstPortfolio",
+      img: oasis,
+      name: "Oasis",
       summary:
-        "App creata con l'utilizzo di HTML,SCSS e JavaScript. Portfolio implementato con Soft skill, Hard skill e un form di contatto.",
-      git: "https://github.com/William-95/william-95.github.io",
-      link: "https://william-95.github.io/",
+        "App creata con l'utilizzo di HTML,CSS, React.js e Laravel. Effettuato il login/registrazione è possibile visionare i cani abbandonati nei vari canili nazionale, l'inserimento di essi nei database o la ricerca di un singolo cane tramite microchip.",
+      git: "https://github.com/William-95/Oasis",
+      link: "https://oasis-23.netlify.app/",
     },
     {
       id: 1,
-      img: counter,
-      name: "Balls-Counter",
-      summary:
-        "App creata con l'utilizzo di HTML,CSS e JavaScript. Un semplice contatore di palline con la presenza dei tasti '+' , '-' e  'Reset'.",
-      git: "https://github.com/William-95/balls-counter.github.io",
-      link: "https://balls-counter.netlify.app/",
-    },
-    {
-      id: 2,
-      img: where,
-      name: "WhereDoYouLive",
-      summary:
-        "App creata con l'utilizzo di HTML,CSS e JavaScript.Tramite la ricerca per nome delle città viene mostrata la qualità della vita e il punteggi della città secondo la classifica di Teleport.",
-      git: "https://github.com/William-95/Where-DoYouLive.github.io",
-      link: "https://where-doyoulive.netlify.app/",
-    },
-    {
-      id: 3,
-      img: enjoy,
-      name: "EnjoyVeg",
-      summary:
-        "App creata con l'utilizzo di HTML,CSS, JavaScript e React.js. App utilizzata per la ricerca di ricette vegetariane attraverso l'applicazione di filtri in base alle proprie preferenze.",
-      git: "https://github.com/William-95/EnjoyVeg",
-      link: "https://enjoyveg.netlify.app/",
-    },
-    {
-      id: 4,
       img: owly,
       name: "Owly-Db",
       summary:
@@ -120,13 +93,40 @@ export default function SetContext({ children }) {
       link: "https://github.com/William-95/owly-db",
     },
     {
-      id: 5,
-      img: oasis,
-      name: "Oasis",
+      id: 2,
+      img: enjoy,
+      name: "EnjoyVeg",
       summary:
-        "App creata con l'utilizzo di HTML,CSS, React.js e Laravel. Effettuato il login/registrazione è possibile visionare i cani abbandonati nei vari canili nazionale, l'inserimento di essi nei database o la ricerca di un singolo cane tramite microchip.",
-      git: "https://github.com/William-95/Oasis",
-      link: "https://oasis-23.netlify.app/",
+        "App creata con l'utilizzo di HTML,CSS, JavaScript e React.js. App utilizzata per la ricerca di ricette vegetariane attraverso l'applicazione di filtri in base alle proprie preferenze.",
+      git: "https://github.com/William-95/EnjoyVeg",
+      link: "https://enjoyveg.netlify.app/",
+    },
+    {
+      id: 3,
+      img: where,
+      name: "WhereDoYouLive",
+      summary:
+        "App creata con l'utilizzo di HTML,CSS e JavaScript.Tramite la ricerca per nome delle città viene mostrata la qualità della vita e il punteggi della città secondo la classifica di Teleport.",
+      git: "https://github.com/William-95/Where-DoYouLive.github.io",
+      link: "https://where-doyoulive.netlify.app/",
+    },
+    {
+      id: 4,
+      img: counter,
+      name: "Balls-Counter",
+      summary:
+        "App creata con l'utilizzo di HTML,CSS e JavaScript. Un semplice contatore di palline con la presenza dei tasti '+' , '-' e  'Reset'.",
+      git: "https://github.com/William-95/balls-counter.github.io",
+      link: "https://balls-counter.netlify.app/",
+    },
+    {
+      id: 5,
+      img: portfolio,
+      name: "FirstPortfolio",
+      summary:
+        "App creata con l'utilizzo di HTML,SCSS e JavaScript. Portfolio implementato con Soft skill, Hard skill e un form di contatto.",
+      git: "https://github.com/William-95/william-95.github.io",
+      link: "https://william-95.github.io/",
     },
   ]);
   // end state project
